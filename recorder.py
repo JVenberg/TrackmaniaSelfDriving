0ce50d38187acc157d2507de79ac174eabc901ee
JVenberg <jvenberg@gmail.com>
2021-05-23 20:54:40 -0700

    Added framerate limit on recorder

--- recorder.py
+++ recorder.py
@@ -13,7 +13,7 @@ from PIL import Image
 
 HOST = '127.0.0.1'
 PORT = 65432
-
+MAX_FPS = 10
 
 class XboxController(object):
     MAX_TRIG_VAL = 256
@@ -121,12 +121,12 @@ class XboxController(object):
 class SaveBuffer():
     def __init__(self, buffer_size, save_dir, img_type='png', data_file_name='telemetry.csv'):
         self.buffer_size = buffer_size
-        self.buffer = deque
+        self.buffer = deque()
         self.save_dir = save_dir
         self.img_type = img_type
 
         data_file_exists = os.path.isfile(save_dir + data_file_name)
-        self.data_file = open(data_file_name, 'a')
+        self.data_file = open(save_dir + data_file_name, 'a', newline='')
         self.data_file_writer = csv.DictWriter(self.data_file, fieldnames=[
             'img_file',
             'time',
@@ -139,8 +139,8 @@ class SaveBuffer():
 
     def _save(self, data):
         img, time, speed, steering, acceleration = data
-        img_file_name = self.save_dir + str(time) + '.' + self.img_type
-        img.save(img_file_name)
+        img_file_name = str(time) + '.' + self.img_type
+        img.save(self.save_dir + img_file_name)
         self.data_file_writer.writerow({
             'img_file': img_file_name,
             'time': time,
@@ -148,6 +148,7 @@ class SaveBuffer():
             'steering': steering,
             'acceleration': acceleration
         })
+        print('Saved:', img_file_name)
 
     def add(self, img, time, speed, steering, acceleration):
         while len(self.buffer) > self.buffer_size:
@@ -159,6 +160,8 @@ class SaveBuffer():
             self._save(self.buffer.pop())
 
     def clear(self):
+        last_time = time.time() - self.buffer.pop()[1] if len(self.buffer) > 0 else 0
+        print('Reverted:', last_time, 'seconds')
         self.buffer.clear()
 
     def close(self):
@@ -178,7 +181,9 @@ if __name__ == '__main__':
         start_stop_down = False
         revert_down = False
 
-        save_buffer = SaveBuffer(10, r'D:/Documents/TrackmaniaSelfDrivingData/', 'png')
+        last_frame_time = time.time()
+
+        save_buffer = SaveBuffer(80, r'D:/Documents/TrackmaniaSelfDrivingData/', 'png')
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((HOST, PORT))
@@ -208,16 +213,21 @@ if __name__ == '__main__':
 
                             lines = conn_file.readlines()
                             last_line = lines[-1] if len(lines) > 0 else False
+                            curr_frame_time = time.time()
                             if last_line and running and not controller_data['pause']:
                                 json_data = json.loads(last_line)
                                 if json_data['speed'] is not None:
-                                    img = d.screenshot(region=(0, 400, width, height - 460)).convert('L')
-                                    img_width, img_height = img.size
-                                    save_buffer.add(
-                                        img.transform((200, 200), Image.QUAD, (500, 0, 0, img_height, img_width, img_height, img_width - 500, 0),
-                                        json_data['speed'],
-                                        controller_data['steering'],
-                                        controller_data['acceleration'])
+                                    if curr_frame_time - last_frame_time >= 1 / MAX_FPS:
+                                        img = d.screenshot(region=(0, 400, width, height - 460)).convert('L')
+                                        img_width, img_height = img.size
+                                        last_frame_time = curr_frame_time
+                                        save_buffer.add(
+                                            img.transform((128, 128), Image.QUAD, (500, 0, 0, img_height, img_width, img_height, img_width - 500, 0)),
+                                            curr_frame_time,
+                                            json_data['speed'],
+                                            controller_data['steering'],
+                                            controller_data['acceleration']
+                                        )
                                 else:
                                     save_buffer.clear()
                 except BlockingIOError:
