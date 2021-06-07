ee39028d2b60a231161852e319bfb791306378b4
JVenberg <jvenberg@gmail.com>
2021-06-06 23:40:27 -0700

    Added speed normalization

--- dataset.py
+++ dataset.py
@@ -8,12 +8,15 @@ from torchvision.io import read_image
 
 import numpy as np
 
+SPEED_SCALE = 400
+
 class TrackManiaDataset(Dataset):
     def __init__(
         self, data_dir, annotations_file_name, transform=None, target_transform=None
     ):
-        self.anno_file = open(os.path.join(data_dir, annotations_file_name))
-        self.img_labels = list(csv.DictReader(self.anno_file))
+        anno_file = open(os.path.join(data_dir, annotations_file_name))
+        self.img_labels = list(csv.DictReader(anno_file))
+        anno_file.close()
         self.data_dir = data_dir
         self.transform = transform
         self.target_transform = target_transform
@@ -25,7 +28,7 @@ class TrackManiaDataset(Dataset):
         row = self.img_labels[idx]
         img_path = os.path.join(self.data_dir, row["img_file"])
         image = read_image(img_path)
-        speed = float(row["speed"]) / 400
+        speed = float(row["speed"]) / SPEED_SCALE
         steering = float(row["steering"])
         if self.transform:
             image = self.transform(image)
@@ -33,7 +36,3 @@ class TrackManiaDataset(Dataset):
             speed = self.target_transform(speed)
             steering = self.target_transform(steering)
         return image, torch.from_numpy(np.array([speed, steering])).float()
-
-    def __del__(self):
-        self.anno_file.close()
-        
