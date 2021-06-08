a9e833e9d4b2d9337eeecd8638ec9c3517ab508f
JVenberg <jvenberg@gmail.com>
2021-06-08 11:04:16 -0700

    Added only steer option to dataset

--- dataset.py
+++ dataset.py
@@ -7,12 +7,13 @@ from torch.utils.data import Dataset
 from torchvision.io import read_image
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 SPEED_SCALE = 400
 
 class TrackManiaDataset(Dataset):
     def __init__(
-        self, data_dir, annotations_file_name, transform=None, target_transform=None
+        self, data_dir, annotations_file_name, only_steer=False, transform=None, target_transform=None
     ):
         anno_file = open(os.path.join(data_dir, annotations_file_name))
         self.img_labels = list(csv.DictReader(anno_file))
@@ -20,6 +21,7 @@ class TrackManiaDataset(Dataset):
         self.data_dir = data_dir
         self.transform = transform
         self.target_transform = target_transform
+        self.only_steer = only_steer
 
     def __len__(self):
         return len(self.img_labels)
@@ -35,4 +37,33 @@ class TrackManiaDataset(Dataset):
         if self.target_transform:
             speed = self.target_transform(speed)
             steering = self.target_transform(steering)
+        
+        if self.only_steer:
+            return image, torch.from_numpy(np.array([steering])).float()
+            
         return image, torch.from_numpy(np.array([speed, steering])).float()
+
+def view_data(data):
+    figure = plt.figure(figsize=(8, 8))
+    cols, rows = 3, 3
+    for i in range(1, cols * rows + 1):
+        sample_idx = torch.randint(len(data), size=(1,)).item()
+        img, speed, steering = data[sample_idx]
+        figure.add_subplot(rows, cols, i)
+        plt.title("Speed: " + str(speed) + "\nSteering: " + str(steering))
+        plt.axis("off")
+        plt.imshow(img.squeeze(), cmap="gray")
+    plt.show()
+
+
+def view_dataloader(dataloader):
+    train_features, train_speed, train_steering = next(iter(dataloader))
+    print(f"Feature batch shape: {train_features.size()}")
+    print(f"Speed batch shape: {train_speed.size()}")
+    print(f"Steering batch shape: {train_steering.size()}")
+    img = train_features[0].squeeze()
+    speed = train_speed[0]
+    steering = train_steering[0]
+    plt.imshow(img, cmap="gray")
+    print(f"Speed: {speed} Steering: {steering}")
+    plt.show()
